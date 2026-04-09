0aa4f4925e97ccf6b76b8ba617f4aeba3d41f133
arkan.alexei <arkan.alexei@ui.ac.id>
2026-04-10 01:05:14 +0200

    feat: add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,28 @@ plugins {
     kotlin("jvm") version "2.3.10"
 }
 
-group = "org.example"
+group = "downloader"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+val ktorVersion: String by project
+val mockkVersion: String by project
+
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("app.cash.turbine:turbine:1.2.1")
+    testImplementation("io.mockk:mockk:${mockkVersion}")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+    implementation("io.ktor:ktor-client-core:${ktorVersion}")
+    implementation("io.ktor:ktor-client-cio:${ktorVersion}")
+    testImplementation("io.ktor:ktor-client-mock:${ktorVersion}")
+    implementation("io.ktor:ktor-server-netty:${ktorVersion}")
+    implementation("io.ktor:ktor-server-partial-content:${ktorVersion}")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
 }
 
 kotlin {
